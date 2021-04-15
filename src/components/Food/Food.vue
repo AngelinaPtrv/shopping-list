5f1600ed27a16e3c71baa1518ac350ba09285daa
Angelina <angelshakirova@gmail.com>
2021-04-15 15:14:35 +0300

    route

---
+++ src/components/Food/Food.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-container>
+    <v-container class="modify d-flex justify-space-around">
+      <!--      <v-text-field-->
+      <!--          class="flex-grow-0 flex-shrink-0"-->
+      <!--          v-model="search"-->
+      <!--          append-icon="mdi-magnify"-->
+      <!--          label="Search"-->
+      <!--      />-->
+      <SearchPanel :list="list" :search="search" @toGetFind="getSearch"></SearchPanel>
+      <v-btn
+          depressed
+          @click="toSortList"
+      >По имени
+      </v-btn>
+    </v-container>
+    <section class="d-flex">
+      <TableList
+          :list="list"
+          :search="search"
+      ></TableList>
+    </section>
+    <section class="new d-flex justify-space-around">
+      <AddPanel :list="list"></AddPanel>
+    </section>
+  </v-container>
+</template>
+
+<script>
+import SearchPanel from "@/components/SearchPanel"
+import TableList from "@/components/TableList";
+import AddPanel from "@/components/AddPanel";
+
+export default {
+  name: "Products",
+  components: {
+    SearchPanel,
+    TableList,
+    AddPanel
+  },
+
+  data () {
+    return {
+      list: [
+        {checked: false, name: "Хлеб", quantity: 1, units: "шт"},
+        {checked: false, name: "Помидоры", quantity: 2, units: "кг"},
+        {checked: false, name: "Огурцы", quantity: 2, units: "кг"},
+        {checked: false, name: "Пиво", quantity: 6, units: "бан"},
+      ],
+      meta: [
+        {field: "checked", type: "bool"},
+        {field: "name", type: "text"},
+        {field: "quantity", type: "number"},
+        {field: "units", type: "combo", refType: "text"}
+      ],
+      headers: [
+        {text: "Наименование", value: "name"},
+        {text: "Количество", value: "quantity"},
+        {text: "Ед. изм", value: "units"},
+      ],
+      search: "",
+      order: 1,
+      snake: false,
+      snackText: '',
+      singleSelect: true,
+      checked: false,
+      color: 'black',
+    }
+  },
+
+  computed: {
+    foundItem() {
+      return this.list.filter((item) => {
+            return item.name && item.name.toLowerCase().includes(this.search.toLowerCase())
+          }
+      );
+    },
+  },
+
+  methods: {
+    getSearch(find) {
+      console.log(this.search);
+      this.search = find;
+    },
+
+    toSortList() {
+      if (this.order) {
+        this.list.sort((a, b) => this.toCompareList(a, b));
+        this.order = 0;
+      } else {
+        this.list.sort((a, b) => this.toCompareList(b, a));
+        this.order = 1;
+      }
+    },
+
+    toCompareList(a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name === b.name) {
+        return 0;
+      }
+      return -1;
+    },
+
+    log(data) {
+      console.log(data);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Lists</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item
+              v-for="link in links"
+              :key="link.title"
+              :to="link.url"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {
+      drawer: false,
+      group: null,
+      links: [
+        {title: 'Home', icon: 'mdi-home', url: '/home'},
+        {title: 'Food', icon: 'mdi-food', url: '/food'},
+        {title: 'Car', icon: 'mdi-car', url: '/car'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
